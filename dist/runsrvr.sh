f0caafc8fd03d365cd307b242ab3a925fec96a51
Anton GLADYR <aglady@lab2-14.CS.McGill.CA>
2020-11-23 13:00:32 -0500

    initial project setup

---
+++ dist/runsrvr.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+if [[ -z "$ZOOBINDIR" ]]
+then
+	echo "Error!! ZOOBINDIR is not set" 1>&2
+	exit 1
+fi
+
+. $ZOOBINDIR/zkEnv.sh
+
+#TODO Include your ZooKeeper connection string here. Make sure there are no spaces.
+# 	Replace with your server names and client ports.
+#export ZKSERVER=lab1.cs.mcgill.ca:21803,lab2.cs.mcgill.ca:21803,lab3.cs.mcgill.ca:21803
+export ZKSERVER=lab2-22.cs.mcgill.ca:21803,lab2-22.cs.mcgill.ca:21803,lab2-44.cs.mcgill.ca:21803
+
+
+java -cp $CLASSPATH:../task:.: DistProcess 
